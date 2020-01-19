edf136c503e8fd1f564879c99048323c18b31ae6
Jody Russell <missjodyrussell@gmail.com>
2020-01-19 11:07:25 -0700

    Working with bank app example to create multiple question paths

--- app.js
+++ app.js
@@ -1,8 +1,67 @@
+//experiment working off of bank app example tuesday the 7th
+
+const inquirer = require("inquirer");
+
+const templateStart = () => {
+  inquirer.prompt([
+    //takes in name 
+    {
+      type: "input",
+      message: "What is your manager's name?",
+      name: "name"
+    },
+    //takes in id, if it's NaN it just lets you continue with NaN, no catch
+    {
+      type: "number",
+      message: "What is your manager's ID number?",
+      name: "id"
+      //validate: function()
+    },
+    //takes in email
+    {
+      type: "input",
+      message: "What is your manager's email address?",
+      name: "email"
+    },
+    //takes in phonenumber
+    {
+      type: "number",
+      message: "What is your manager's office phone number?",
+      name: "officeNumber"
+    },
+    {
+      type: "list",
+      message: "Which type of team member would you like to add now?",
+      choices: ["Engineer", "Intern", "I don't want to add any more team members, thanks!"],
+      name: "action"
+    }
+  ]).then((prompt) => {
+    //send prompt to make "new Manager"
+    // switch (prompt.action) {
+    //   case "Engineer":
+    //     engineerStart();
+    //     break;
+    //   case "Intern": 
+    //     internStart();
+    //     break;
+    //   default:
+    //     htmlRenderer();
+    //     break;
+    // }
+  });
+};
+
+templateStart();
+
+// ^ 1/19 1056 experiment
+//////////////
+// v before 1056
+
 // const Manager = require("./lib/Manager");
 // const Engineer = require("./lib/Engineer");
 // const Intern = require("./lib/Intern");
 
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 // const path = require("path");
 // const fs = require("fs");
 
@@ -10,64 +69,64 @@ const inquirer = require("inquirer");
 
 // const render = require("./lib/htmlRenderer");
 
+//reusable function
+// const validateId = id => (id <=0 || isNaN(id)) ? "Please enter a four digit employee ID number" : true;
+
 // Welcome message
 // do we need to set it up that welcomeMessage then calls managerPrompt so that
 // they don't go out of order ever?
 // const welcomeMessage = 
+// console.log("Please build your engineering team: ");
 
-console.log("Please build your engineering team: ");
 
 ///////
 
 // First Prompt
-const managerPrompts = [
-  //takes in name 
-  {
-    type: "input",
-    message: "What is your manager's name?",
-    name: "name"
-  },
-  //takes in id, if it's NaN it just lets you continue with NaN, no catch
-  {
-    type: "number",
-    message: "What is your manager's ID number?",
-    name: "id"
-    //validate: function()
-  },
-  //takes in email
-  {
-    type: "input",
-    message: "What is your manager's email address?",
-    name: "email"
-  },
-  //takes in phonenumber
-  {
-    type: "number",
-    message: "What is your manager's office phone number?",
-    name: "officeNumber"
-  }
-];
-
-
-// .then(function (response) {
-
-//uses this information to create new Manager
-//passes Manager to html generation
-// ends series of questions and heads to the next prompt
-// teamPrompt(response);
-// });
-
-inquirer.prompt(managerPrompts);
+// const managerPrompts = [
+  // //takes in name 
+  // {
+  //   type: "input",
+  //   message: "What is your manager's name?",
+  //   name: "name"
+  // },
+  // //takes in id, if it's NaN it just lets you continue with NaN, no catch
+  // {
+  //   type: "number",
+  //   message: "What is your manager's ID number?",
+  //   name: "id"
+  //   //validate: function()
+  // },
+  // //takes in email
+  // {
+  //   type: "input",
+  //   message: "What is your manager's email address?",
+  //   name: "email"
+  // },
+  // //takes in phonenumber
+  // {
+  //   type: "number",
+  //   message: "What is your manager's office phone number?",
+  //   name: "officeNumber"
+  // }
+// ];
 
 
 ///////
 
 // Second Prompt
+// const secondPrompt = [
+//   // > Engineer
+//   // > Intern
+//   // > I don't want to add any more team members, thanks
+  // {
+  //   type: "list",
+  //   message: "Which type of team member would you like to add now?",
+  //   choices: ["Engineer", "Intern", "I don't want to add any more team members, thanks!"]
+  // }
+// ];
 
-//Which type of team member would you like to add now? 
-// > Engineer
-// > Intern
-// > I don't want to add any more team members, thanks
+
+
 
 // Sub prompt: Engineer
 
@@ -98,7 +157,22 @@ inquirer.prompt(managerPrompts);
 
 
 
-// call prompts in order?
-// welcomeMessage();
-// managerPrompt();
+// inquirer.prompt(managerPrompts);
 
+// .then(function (response) {  
+//uses this information to create new Manager
+//passes Manager to html generation
+// ends series of questions and heads to the next prompt
+// teamPrompt(response);
+// });
+
+// inquirer.prompt(secondPrompt);
+
+// > If Engineer
+//    call Engineer prompts
+//    that in turn then calls secondPrompt again
+// > Intern
+//    call Intern prompts
+//    that in turn calls secondPrompt again
+// > I don't want to add any more team members, thanks
+//    calls the HTML generation and displays a message about that biz
