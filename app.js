217ef77354ad0e61696c7e05ee692d947a96c767
Jody Russell <missjodyrussell@gmail.com>
2020-01-19 09:18:55 -0700

    first msg and prompt wired up

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const outputPath = path.resolve(__dirname, "output", "team.html");
+// const outputPath = path.resolve(__dirname, "output", "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+// Welcome message
+// do we need to set it up that welcomeMessage then calls managerPrompt so that
+// they don't go out of order ever?
+// const welcomeMessage = 
+
+console.log("Please build your engineering team: ");
 
 ///////
 
 // First Prompt
+const managerPrompts = [
+  //takes in name 
+  {
+    type: "input",
+    message: "What is your manager's name?",
+    name: "name"
+  },
+  //takes in id, if it's NaN it just lets you continue with NaN, no catch
+  {
+    type: "number",
+    message: "What is your manager's ID number?",
+    name: "id"
+    //validate: function()
+  },
+  //takes in email
+  {
+    type: "input",
+    message: "What is your manager's email address?",
+    name: "email"
+  },
+  //takes in phonenumber
+  {
+    type: "number",
+    message: "What is your manager's office phone number?",
+    name: "officeNumber"
+  }
+];
 
-//Please build your engineering team:
-//What is your manager's name?
-//takes in name 
-//What is your manager's ID number? 
-//takes in id
-//What is your manager's email address? 
-//takes in email
-//What is your manager's office phone number?
-//takes in phonenumber
+
+// .then(function (response) {
 
 //uses this information to create new Manager
-
 //passes Manager to html generation
-
 // ends series of questions and heads to the next prompt
+// teamPrompt(response);
+// });
+
+inquirer.prompt(managerPrompts);
+
 
 ///////
 
@@ -39,32 +69,36 @@ const render = require("./lib/htmlRenderer");
 // > Intern
 // > I don't want to add any more team members, thanks
 
-  // Sub prompt: Engineer
+// Sub prompt: Engineer
 
-  //What is your engineer's name?
-  //takes in name 
-  //What is your engineer's ID number? 
-  //takes in id
-  //What is your engineer's email address? 
-  //takes in email
-  //What is your engineer's GitHub user name?
-  //takes in github
+//What is your engineer's name?
+//takes in name 
+//What is your engineer's ID number? 
+//takes in id
+//What is your engineer's email address? 
+//takes in email
+//What is your engineer's GitHub user name?
+//takes in github
 
-  // Sub prompt: Intern
+// Sub prompt: Intern
 
-  //What is your intern's name?
-  //takes in name 
-  //What is your intern's ID number? 
-  //takes in id
-  //What is your intern's email address? 
-  //takes in email
-  //Where is your intern attending school?
-  //takes in github
+//What is your intern's name?
+//takes in name 
+//What is your intern's ID number? 
+//takes in id
+//What is your intern's email address? 
+//takes in email
+//Where is your intern attending school?
+//takes in github
 
-  // Sub prompt : I don't wanna
+// Sub prompt : I don't wanna
 
-  // break out and generate html
-  // console log that it is available to view
+// break out and generate html
+// console log that it is available to view
 
-  //
+
+
+// call prompts in order?
+// welcomeMessage();
+// managerPrompt();
 
